ed846e4841435a2668c423dfb75006c7b2aac773
Roman Atramonov <r.artamonov@dssl.ru>
2021-06-02 22:08:28 +0300

    change

--- hikisapi.py
+++ hikisapi.py
@@ -3,6 +3,7 @@ from requests.auth import HTTPDigestAuth
 import xmltodict
 from http import HTTPStatus
 import const
+import settings
 
 class Hikvision:
 
@@ -12,22 +13,21 @@ class Hikvision:
         self.user = user
         self.paswd = paswd
         self.port = port
-         
+        HOST = f'http://{self.ipaddr}:{self.port}/ISAPI' 
     
     def __repr__(self):
         
         return f"<Device adress: {self.ipaddr}, user: {self.user}, password: {self.paswd}, port: {self.port}>"
 
-    def __sent_request(self, method, path):
-        
-        HOST = f'http://{self.ipaddr}:{self.port}/ISAPI'
+    def __send_request(self, method, path):
+                
         method = getattr(requests, method) 
-        incoming_request = method(f'{HOST}{path}', auth=HTTPDigestAuth(self.user, self.paswd))
+        incoming_request = method(f'{self.HOST}{path}', auth=HTTPDigestAuth(self.user, self.paswd))
         return incoming_request
         
     def check_device_status(self):
         
-        device_status = self.__sent_request('get', '/System/status').status_code
+        device_status = self.__send_request('get', '/System/status').status_code
         if device_status == HTTPStatus.OK:
             return 'OK'
         elif device_status == HTTPStatus.BAD_REQUEST: 
@@ -45,13 +45,13 @@ class Hikvision:
 
     def get_model_name(self):
         
-        device_info = self.__sent_request('get','/System/deviceinfo')
+        device_info = self.__send_request('get','/System/deviceinfo')
         all_device_info = xmltodict.parse(device_info.text)
         return all_device_info['DeviceInfo']['model']
     
     def get_device_capabilities(self, stream=const.MAIN):  #MAIN - основной поток камеры. SUB - субпоток
         
-        device_capabilities = self.__sent_request('get', f'/Streaming/channels/{stream}/capabilities')
+        device_capabilities = self.__send_request('get', f'/Streaming/channels/{stream}/capabilities')
         device_capabilities = xmltodict.parse(device_capabilities.text)
         # берем из полученной xml только нужные параметры
         capabilities = device_capabilities['StreamingChannel']['Video']
@@ -61,12 +61,12 @@ class Hikvision:
         fps = capabilities['maxFrameRate']['@opt'].split(',')
         codecs = capabilities['videoCodecType']['@opt'].split(',')
         audio_codecs = device_capabilities['StreamingChannel']['Audio']['audioCompressionType']['@opt'].split(',')
-        dict_with_useful_capabilities = { 'resolutions' : resolutions,
+        useful_capabilities = { 'resolutions' : resolutions,
                                           'codecs' : codecs,
                                           'fps' : fps,
                                           'audio_codecs' : audio_codecs
                                         }
-        return dict_with_useful_capabilities
+        return useful_capabilities
     
     def set_device_settings(self, codec, resolution, fps, audio_codec):
       #  device_capabilities = get_device_capabilities()
@@ -90,7 +90,7 @@ class Hikvision:
         pass
 
     def restore_to_default(self):
-        restoring = self.__sent_request('put', '/System/factoryReset')
+        restoring = self.__send_request('put', '/System/factoryReset')
                                
         if restoring.status_code == HTTPStatus.OK:
             return "Устройство сбрасывается до заводских настроек. Не перезагружайте камеру."
@@ -98,7 +98,7 @@ class Hikvision:
             return f"Что-то пошло не так. Камера вернула {restoring.status_code}"
 
     def reboot(self):
-        rebooting = self.__sent_request('put','/System/reboot')
+        rebooting = self.__send_request('put','/System/reboot')
         if rebooting.status_code == HTTPStatus.OK:
             return "Устройство перезагружается"
         else:
@@ -111,7 +111,7 @@ class Hikvision:
         pass
 
     def get_device_config(self):
-        device_config = self.__sent_request('get','/System/configurationData')
+        device_config = self.__send_request('get','/System/configurationData')
         with open('configuration_data', 'wb') as config:
             config.write(device_config.content)
         return 'Конфигурация скопирована'
@@ -124,7 +124,7 @@ class Hikvision:
 
 
 if __name__ == "__main__":
-    a = Hikvision('172.16.13.70', 'admin', 'Admin321678')
+    a = Hikvision(settings.ipaddr, settings.user, settings.paswd)
     print(a.get_device_capabilities())
     #print(a.get_device_capabilities())
     
---
+++ settings.py
@@ -0,0 +1,4 @@
+ipaddr='172.16.13.70'
+user='admin' 
+paswd='Admin1337' 
+port=80
