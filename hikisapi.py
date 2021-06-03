2877388f89425da7256fe295f4aea1f6494cb263
Roman Artamonov <r.artamonov@dssl.ru>
2021-06-03 17:43:43 +0300

    add datetime and set_device_settings def

--- hikisapi.py
+++ hikisapi.py
@@ -13,23 +13,23 @@ class Hikvision:
         self.user = user
         self.paswd = paswd
         self.port = port
-        HOST = f'http://{self.ipaddr}:{self.port}/ISAPI' 
+        self.HOST = f'http://{self.ipaddr}:{self.port}/ISAPI' 
     
     def __repr__(self):
         
         return f"<Device adress: {self.ipaddr}, user: {self.user}, password: {self.paswd}, port: {self.port}>"
 
-    def __send_request(self, method, path):
+    def __send_request(self, method, path, data=None):
                 
-        method = getattr(requests, method) 
-        incoming_request = method(f'{self.HOST}{path}', auth=HTTPDigestAuth(self.user, self.paswd))
+        method = getattr(requests, method, data) 
+        incoming_request = method(f'{self.HOST}{path}', auth=HTTPDigestAuth(self.user, self.paswd), data=data)
         return incoming_request
         
     def check_device_status(self):
         
         device_status = self.__send_request('get', '/System/status').status_code
         if device_status == HTTPStatus.OK:
-            return 'OK'
+            return HTTPStatus.OK
         elif device_status == HTTPStatus.BAD_REQUEST: 
             raise ConnectionError("Bad request")
         elif device_status == HTTPStatus.UNAUTHORIZED:
@@ -44,35 +44,51 @@ class Hikvision:
             raise ConnectionError("Not Implemented")
 
     def get_model_name(self):
-        
-        device_info = self.__send_request('get','/System/deviceinfo')
-        all_device_info = xmltodict.parse(device_info.text)
-        return all_device_info['DeviceInfo']['model']
+
+        if self.check_device_status() == HTTPStatus.OK:
+            device_info = self.__send_request('get','/System/deviceinfo')
+            all_device_info = xmltodict.parse(device_info.text)
+            return all_device_info['DeviceInfo']['model']
+        else:
+            return self.check_device_status()
     
     def get_device_capabilities(self, stream=const.MAIN):  #MAIN - основной поток камеры. SUB - субпоток
         
-        device_capabilities = self.__send_request('get', f'/Streaming/channels/{stream}/capabilities')
-        device_capabilities = xmltodict.parse(device_capabilities.text)
-        # берем из полученной xml только нужные параметры
-        capabilities = device_capabilities['StreamingChannel']['Video']
-        width = capabilities['videoResolutionWidth']['@opt'].split(',')
-        height = capabilities['videoResolutionHeight']['@opt'].split(',')
-        resolutions = [[w + 'x' + h] for w, h in zip(width, height)]  # чтобы разрешение было в понятном виде
-        fps = capabilities['maxFrameRate']['@opt'].split(',')
-        codecs = capabilities['videoCodecType']['@opt'].split(',')
-        audio_codecs = device_capabilities['StreamingChannel']['Audio']['audioCompressionType']['@opt'].split(',')
-        useful_capabilities = { 'resolutions' : resolutions,
-                                          'codecs' : codecs,
-                                          'fps' : fps,
-                                          'audio_codecs' : audio_codecs
-                                        }
-        return useful_capabilities
+        if self.check_device_status() == HTTPStatus.OK:
+            device_capabilities = self.__send_request('get', f'/Streaming/channels/{stream}/capabilities')
+            device_capabilities = xmltodict.parse(device_capabilities.text)
+            # берем из полученной xml только нужные параметры
+            capabilities = device_capabilities['StreamingChannel']['Video']
+            width = capabilities['videoResolutionWidth']['@opt'].split(',')
+            height = capabilities['videoResolutionHeight']['@opt'].split(',')
+            resolutions = [[w + 'x' + h] for w, h in zip(width, height)]  # чтобы разрешение было в понятном виде
+            fps = capabilities['maxFrameRate']['@opt'].split(',')
+            codecs = capabilities['videoCodecType']['@opt'].split(',')
+            audio_codecs = device_capabilities['StreamingChannel']['Audio']['audioCompressionType']['@opt'].split(',')
+            useful_capabilities = { 'resolutions' : resolutions,
+                                              'codecs' : codecs,
+                                              'fps' : fps,
+                                              'audio_codecs' : audio_codecs
+                                            }
+            return useful_capabilities
+        else:
+            return self.check_device_status()   
     
-    def set_device_settings(self, codec, resolution, fps, audio_codec):
-      #  device_capabilities = get_device_capabilities()
-      #  device_setting = device_settings.device_config
-       # return device_setting
-       pass
+    def set_device_settings(self, codec, resolution, fps, stream=const.MAIN):
+        
+        if self.check_device_status() == HTTPStatus.OK:
+            width, height = resolution.split('x')
+            default_settings = xmltodict.parse(settings.device_settings)
+            inside_xml_video = default_settings['StreamingChannel']['Video']
+            inside_xml_video['videoCodecType'] = codec
+            inside_xml_video['videoResolutionWidth'] = width 
+            inside_xml_video['videoResolutionHeight'] = height
+            inside_xml_video['maxFrameRate'] = fps
+            our_settings = xmltodict.unparse(default_settings).replace('\n','')   # unparse возвращает xml c \n в запросе. Такой запрос камера не принимает
+            self.__send_request('put', f'/Streaming/channels/{stream}/', our_settings)
+            return 'Настройки успешно применились'
+        else:
+            return self.check_device_status() 
 
     def set_image_settings(self):
         pass
@@ -80,7 +96,18 @@ class Hikvision:
     def set_network_settings(self):
         pass
 
-    def set_datetime(self):
+    def set_datetime_manual(self, user_date_time):
+        
+        if self.check_device_status() == HTTPStatus.OK:
+            datetime_settings = xmltodict.parse(settings.time_settings)
+            datetime_settings['Time']['localTime'] = user_date_time
+            our_time_settings = xmltodict.unparse(datetime_settings)
+            self.__send_request('put', f'/System/time', our_time_settings)
+            return our_time_settings
+        else:
+            return self.check_device_status()
+            
+    def set_datetime_by_ntp(self):
         pass
 
     def set_motion_detector(self):
@@ -90,19 +117,27 @@ class Hikvision:
         pass
 
     def restore_to_default(self):
-        restoring = self.__send_request('put', '/System/factoryReset')
-                               
-        if restoring.status_code == HTTPStatus.OK:
-            return "Устройство сбрасывается до заводских настроек. Не перезагружайте камеру."
+
+        if self.check_device_status() == HTTPStatus.OK:
+            restoring = self.__send_request('put', '/System/factoryReset')
+            if restoring.status_code == HTTPStatus.OK:
+                return "Устройство сбрасывается до заводских настроек. Не перезагружайте камеру."
+            else:
+                return f"Что-то пошло не так. Камера вернула {restoring.status_code}"
         else:
-            return f"Что-то пошло не так. Камера вернула {restoring.status_code}"
+            self.check_device_status() 
 
     def reboot(self):
-        rebooting = self.__send_request('put','/System/reboot')
-        if rebooting.status_code == HTTPStatus.OK:
-            return "Устройство перезагружается"
+
+        if self.check_device_status() == HTTPStatus.OK:    
+            rebooting = self.__send_request('put','/System/reboot')
+            if rebooting.status_code == HTTPStatus.OK:
+                return "Устройство перезагружается"
+            else:
+                return f"Что-то пошло не так. Камера вернула {rebooting.status_code}"
         else:
-            return f"Что-то пошло не так. Камера вернула {rebooting.status_code}"
+            self.check_device_status() 
+        
     
     def get_events(self):
        # events = requests.get(f'http://{self.ipaddr}:{self.port}/ISAPI/Event/notification/alertStream',
@@ -111,10 +146,15 @@ class Hikvision:
         pass
 
     def get_device_config(self):
-        device_config = self.__send_request('get','/System/configurationData')
-        with open('configuration_data', 'wb') as config:
-            config.write(device_config.content)
-        return 'Конфигурация скопирована'
+       
+        if self.check_device_status() == HTTPStatus.OK:    
+            device_config = self.__send_request('get','/System/configurationData')
+            with open('configuration_data', 'wb') as config:
+                config.write(device_config.content)
+            return 'Конфигурация скопирована'
+        else:
+            self.check_device_status() 
+        
 
     def set_device_config(self):
         pass
@@ -125,7 +165,8 @@ class Hikvision:
 
 if __name__ == "__main__":
     a = Hikvision(settings.ipaddr, settings.user, settings.paswd)
-    print(a.get_device_capabilities())
+    #print(a.set_device_settings('H.264', '1280x720', '2000'))
     #print(a.get_device_capabilities())
-    
-    
+    #print(a.get_model_name())
+    #print(settings.time_settings)
+    print(a.set_datetime_manual('2021-07-04T16:06:12'))
--- settings.py
+++ settings.py
@@ -1,4 +1,44 @@
 ipaddr='172.16.13.70'
 user='admin' 
-paswd='Admin1337' 
+paswd='Admin321678' 
 port=80
+
+device_settings = """ 
+<StreamingChannel>
+<id>101</id>
+<channelName>IPdome</channelName>
+<enabled>true</enabled>
+    <Video>
+        <enabled>true</enabled>
+        <videoInputChannelID>1</videoInputChannelID>
+        <videoCodecType>H.264</videoCodecType>
+        <videoScanType>progressive</videoScanType>
+        <videoResolutionWidth>1</videoResolutionWidth>
+        <videoResolutionHeight>1</videoResolutionHeight>
+        <videoQualityControlType>VBR</videoQualityControlType>
+        <fixedQuality>100</fixedQuality>
+        <vbrUpperCap>4096</vbrUpperCap>
+        <vbrLowerCap>32</vbrLowerCap>
+        <maxFrameRate>2500</maxFrameRate>
+        <keyFrameInterval>800</keyFrameInterval>
+        <snapShotImageType>JPEG</snapShotImageType>
+        <H264Profile>Main</H264Profile>
+        <GovLength>20</GovLength>
+    </Video>
+</StreamingChannel>
+"""
+
+time_settings= '''<Time>
+<timeMode>manual</timeMode>
+<localTime>2021-06-03 16:06:12</localTime>
+<timeZone>CST-3:00:00</timeZone>
+</Time>'''
+
+ntp_settings = '''<NTPServer>
+<id>1</id>
+<addressingFormatType>hostname</addressingFormatType>
+<hostName>time.windows.com</hostName>
+<portNo>123</portNo>
+<synchronizeInterval>1440</synchronizeInterval>
+</NTPServer>
+'''
