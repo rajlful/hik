f6f7004fd83ae7e5a576039237449802708cefd3
Roman Atramonov <r.artamonov@dssl.ru>
2021-07-08 10:18:02 +0300

    Player

--- GUI.py
+++ GUI.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from PIL import ImageTk, Image
+import cv2
 
 from hikisapi import Hikvision
 from hikdb import Hikdb
@@ -7,39 +9,38 @@ class MainWindow(Tk):
     
     def __init__(self):
         super().__init__()
-        self.main_window = Tk()
-        self.main_window.title("HIKVISION VMS")
-        self.main_window.geometry('1100x700')
+        self.title("HIKVISION VMS")
+        self.geometry('1100x700')
 
-        self.cameras_list = Listbox(self.main_window, width=23, height=20)
+        self.cameras_list = Listbox(self, width=23, height=20)
         self.cameras_list.place(x=2, y=30)
-        self.cameras_list_label = Label(self.main_window, text='List of cameras', bg='white', font="Arial 8")
+        self.cameras_list_label = Label(self, text='List of cameras', bg='white', font="Arial 8")
         self.cameras_list_label.place(x=5, y=35)
-        self.scrollbar = Scrollbar(self.main_window)
+        self.scrollbar = Scrollbar(self)
         self.scrollbar.config(command=self.cameras_list.yview)
 
-        self.journal_events = Listbox(self.main_window, width=23, height=18)
+        self.journal_events = Listbox(self, width=23, height=18)
         self.journal_events.place(x=2, y=385)
-        self.scrollbar = Scrollbar(self.main_window)
+        self.scrollbar = Scrollbar(self)
         self.scrollbar.config(command=self.journal_events.yview)
 
-        self.add_device_btn = Button(self.main_window, text="Add device", command=self.add_device_window)
+        self.add_device_btn = Button(self, text="Add device", command=self.add_device_window)
         self.add_device_btn.place(x=0, y=1)
 
-        self.del_device_btn = Button(self.main_window, text="Delete device", command=self.remove_device)
+        self.del_device_btn = Button(self, text="Delete device", command=self.remove_device)
         self.del_device_btn.place(x=75, y=1)
 
-        self.events_btn = Button(self.main_window, width=20, height=1, text="Show events", command=self.add_motion_events)
+        self.events_btn = Button(self, width=20, height=1, text="Show events", command=self.add_motion_events)
         self.events_btn.place(x=7, y=357)
 
-        self.motion_detector_btn = Button(self.main_window, text="Motion detector", command=self.md_setting)
+        self.motion_detector_btn = Button(self, text="Motion detector", command=self.md_setting)
         self.motion_detector_btn.place(x=175, y=457)
 
-        self.player = Frame(self.main_window, bg='black',width=650, height=450)
+        self.cap = cv2.VideoCapture("rtsp://admin:Admin1337@172.16.13.93:554/h264/ch01/sub/av_stream")
+        self.player = Canvas(self, width=650, height=450)
         self.player.place(x=175, y=5)
-        self.player_label = Label(self.main_window, text="Player will be there...", fg='red', font="Arial 23", bg='black')
-        self.player_label.place(x=350, y=250)
-        
+        self.update_image()
+
     def add_device_window(self):
         
         self.remove_label()
@@ -95,6 +96,20 @@ class MainWindow(Tk):
         self.enable_md_checkbutton.pack(anchor=CENTER)
         self.sensivity_scale = Scale(self.md_window, orient=HORIZONTAL)
         self.sensivity_scale.place(x=70, y=20)
+
+
+    def update_image(self):
+       
+        # Get the latest frame and convert image format
+        self.image = cv2.cvtColor(self.cap.read()[1], cv2.COLOR_BGR2RGB) # to RGB
+        self.image = cv2.resize(self.image, (650,450)) # resize for my canvas
+        self.image = Image.fromarray(self.image) # to PIL format
+        self.image = ImageTk.PhotoImage(self.image, master=self) # to ImageTk format
+        # Update image
+        self.player.create_image(0, 0, anchor=NW, image=self.image)
+        self.after(20, self.update_image)  # 20 - interval in ms to get the latest frame
+       
+       
 
 
 a = MainWindow()
